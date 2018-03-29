08e5e7799b3cab79510d226682ccd906191b5870
Admin <admin@admins-mbp-2.lan>
2018-03-29 00:13:57 -0400

    images added, timer for game added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 //Variables
 var wins = 0;
 var losses = 0;
+var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
 var wordArrayNormal = ("adult scrawny live handle possess babies afraid powerful yard talk guitar dare knock trade cabbage undress swim lush gold vulgar doubt harsh zebra fireman pest potato powder butter screw strong riddle unable plane").split(" ");
 var wordArrayHard = ("smiling diligent victorious scribble tranquil momentous concentrate beneficial butterfly island mundane powerful foregoing unadvised imported alluring innate physical quilt stereotyped unequaled intend arrogant numberless degree apparatus").split(" ");
 var hangmanWordNormal = wordArrayNormal[Math.floor(Math.random() * wordArrayNormal.length)];
@@ -20,6 +21,14 @@ var guessesLeftHard = Math.trunc(hangmanWordHard.length * 1.75);
 var guessesMade = [];
 var currentGuess = null;
 
+//html
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guessesLeftNormal").innerHTML = guessesLeftNormal;
+document.getElementById("normalAnswer").innerHTML = normalAnswer.join(" ");
+document.getElementById("currentGuess").innerHTML = currentGuess;
+document.getElementById("guessesMade").innerHTML = guessesMade;
+
 //Console information
 console.log(hangmanWordNormal);
 console.log(normalAnswer);
@@ -36,7 +45,7 @@ function gameOver() {
     losses++;
     alert("Out Of Guesses! Couldn't Crack The Code! Indy Didn't Escape With The Artifact!");
   };
-  if (gameTimer === 0) {
+  if (timer === 0) {
     losses++;
     alert("Out Of Time! Couldn't Crack The Code! Indy Didn't Escape With The Artifact!")
   };
@@ -47,15 +56,72 @@ function confirmGameOver() {
   return guessesLeftNormal === 0 || gameTimer === 0 || normalAnswer.indexOf("_") === -1;
 };
 
+function confirmLetter(letter) {
+  return alphabet.indexOf(letter) > -1;
+  alert("Not A Letter! Please Choose A Letter A-Z!")
+};
+
+function wrongGuess() {
+  guessesLeftNormal--;
+};
+
+function startGame() {
+  guessesLeftNormal;
+  guessesMade = [];
+  currentGuess = null;
+  normalAnswer;
+};
+
+function startTimer(duration, display) {
+    var timer = duration, mintues, seconds;
+    setInterval(function() {
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+
+};
+
+window.onload = function () {
+  var fiveMinutes = 60 * 5;
+    display = document.querySelector("#timer")
+    startTimer(fiveMinutes, display);
+};
+
+/*function getAlphabet() {
+    var alphabetArray = [];
+    for (var i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++) {
+      alphabetArray.push(String.fromCharCode(i));
+    }
+    return alphabetArray;
+}*/
+
+document.onkeyup = function(event) {
+  var currentGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (guessesMade.indexOf(currentGuess) < 0 && normalAnswer.indexOf(currentGuess) >= 0){
+  guessesMade[guessesMade.length] = currentGuess;
+  guessesLeftNormal--;
+  };
+
+};
 
 
 
 
 
-//html
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("losses").innerHTML = losses;
-document.getElementById("guessesLeftNormal").innerHTML = guessesLeftNormal;
-document.getElementById("normalAnswer").innerHTML = normalAnswer.join(" ");
-document.getElementById("currentGuess").innerHTML = currentGuess;
-document.getElementById("guessesMade").innerHTML = guessesMade;
+
+
+
+
+
+
+
+
+
+//extra elements
